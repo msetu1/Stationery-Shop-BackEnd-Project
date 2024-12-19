b69c92ea1de93821fa54c5c8956378eb60ea1cbb
msetu1 <msetu5763@gmail.com>
2024-12-19 19:00:21 +0600

    error handler and not found error add

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import { ProductRoute } from './module/Product/product.route';
 import { OrderRoute } from './module/Order/order.route';
+import globalErrorHandler from './middlewares/globalErrorHandling';
+import notFound from './middlewares/notFound';
 const app: Application = express();
 
 // parser
@@ -15,5 +17,6 @@ app.use('/api/orders', OrderRoute);
 app.get('/', (req: Request, res: Response) => {
   res.send('Server is running');
 });
-
+app.use(globalErrorHandler);
+app.use(notFound);
 export default app;
